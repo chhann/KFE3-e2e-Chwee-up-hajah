dc4fc5fa5e3f022358d35675c5a333c9705ad4fc
chhann <wjdcks2252@naver.com>
2025-06-20 18:33:17 +0900

    feat: chat 리스트, 방 기본 기능 구현 (#3)

---
+++ apps/web/entities/chat/types.ts
@@ -0,0 +1,10 @@
+export type ChatRoom = {
+  room_id: string;
+  product_id: string;
+  product_name: string;
+  buyer_id: string;
+  buyer_nickname: string;
+  seller_id: string;
+  seller_nickname: string;
+  created_at: string;
+};
