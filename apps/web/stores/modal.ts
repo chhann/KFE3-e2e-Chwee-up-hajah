dcf6495ea7d2074a5a7ff0bb0d10dd02b1c54b10
unknown <kyung3098@naver.com>
2025-06-24 21:37:31 +0900

    fix: 모달 상태 타입 수정

--- apps/web/stores/modal.ts
+++ apps/web/stores/modal.ts
@@ -1,6 +1,6 @@
 import { create } from 'zustand';
 
-type ModalType = 'notification' | 'location' | 'auth' | null;
+type ModalType = 'notification' | 'location' | 'permission' | null;
 
 interface ModalStore {
   openModal: ModalType;
