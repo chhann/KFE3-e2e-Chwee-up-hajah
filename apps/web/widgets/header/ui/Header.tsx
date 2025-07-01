56f3e9dc380efb24eed9fa73b4ab96a8b51fbf93
unknown <kyung3098@naver.com>
2025-07-01 23:04:42 +0900

    feat: header 뒤로가기 버튼 추가

--- apps/web/widgets/header/ui/Header.tsx
+++ apps/web/widgets/header/ui/Header.tsx
@@ -1,22 +1,44 @@
+'use client';
+
 import { Button } from '@repo/ui/design-system/base-components/Button/index';
+import { usePathname, useRouter } from 'next/navigation';
+import { FaArrowLeft } from 'react-icons/fa';
+
 import { useModalStore } from '../../../stores/modal';
 
 export const Header = () => {
+  const pathname = usePathname();
+  const router = useRouter();
   const { setOpenModal } = useModalStore();
 
+  const noBackButtonRoutes = ['/main'];
+  const showBackButton = !noBackButtonRoutes.includes(pathname);
+
   return (
-    <header className="text-neutral-70 flex h-[68px] cursor-pointer items-center justify-end">
-      <Button
-        variants="ghost"
-        size="thinMd"
-        onClick={() => setOpenModal('notification')}
-        className="px-0"
-      >
-        <h2>알림</h2>
-      </Button>
-      <Button size="thinMd" variants="ghost" onClick={() => setOpenModal('location')}>
-        <h2>내 위치</h2>
-      </Button>{' '}
+    <header className="text-neutral-70 flex h-[68px] cursor-pointer items-center justify-between pl-2">
+      {showBackButton && (
+        <div className="text-neutral-700" onClick={() => router.back()}>
+          <FaArrowLeft className="size-4" />
+        </div>
+      )}
+      <div className="grow text-right">
+        <Button
+          variants="ghost"
+          size="thinMd"
+          onClick={() => setOpenModal('notification')}
+          className="mr-1 px-2"
+        >
+          <h2>알림</h2>
+        </Button>
+        <Button
+          size="thinMd"
+          variants="ghost"
+          onClick={() => setOpenModal('location')}
+          className="px-2"
+        >
+          <h2>내 위치</h2>
+        </Button>
+      </div>
     </header>
   );
 };
