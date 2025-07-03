3d2604e5f4d4ec5e6a9d2d5bf01c945f419ad5c2
park-chan-hui <fhfh54321@motionimaging.net>
2025-07-03 20:55:00 +0900

    refactor: 경매 등록 페이지 스타일 상수화 + 관련 공통 컴포넌트 스타일 상수화

---
+++ apps/web/features/auction-add/ui/styles/AuctionImageUploader.styles.ts
@@ -0,0 +1,10 @@
+export const AuctionImageUploaderContainerStyle = 'mb-1 flex items-start gap-2';
+export const ImageUploaderStyle =
+  'w-30 h-30 border-neutral-40 flex shrink-0 cursor-pointer items-center justify-center rounded-md border';
+export const ImageUploaderIconStyle = 'text-neutral-40';
+
+export const ImagePreviewContainerStyle = 'flex max-w-[210px] flex-nowrap gap-2 overflow-x-auto';
+export const ImagePreviewStyle =
+  'w-30 h-30 border-neutral-40 relative shrink-0 overflow-hidden rounded border';
+export const ImageStyle = 'h-full w-full object-cover';
+export const DelateButtonStyle = 'absolute right-0 top-0';
