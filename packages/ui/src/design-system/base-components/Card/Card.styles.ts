0acc40e18d8e2608d9ec10b3fbcc6f904d709968
park-chan-hui <fhfh54321@motionimaging.net>
2025-07-04 19:22:38 +0900

    fix: 누락된 auctionDateSeletor 파일 타입 추가 및 card 충돌로 제거 된 스타일 작성

--- packages/ui/src/design-system/base-components/Card/Card.styles.ts
+++ packages/ui/src/design-system/base-components/Card/Card.styles.ts
@@ -2,5 +2,5 @@ export const cardImageContainerStyle = 'relative';
 export const cardImageStyle = 'h-48 w-full rounded-t-lg object-cover';
 export const cardImageBadgeStyle = 'absolute right-2 top-2';
 export const cardInfoContainerStyle = 'flex items-center justify-between';
-export const cardInfoLeftTimeStyle = 'text-neutral-70 flex items-center gap-1';
+export const cardInfoLeftTimeStyle = 'text-neutral-70 flex items-center gap-1 truncate';
 export const cardTitleStyle = 'text-lg font-semibold';
