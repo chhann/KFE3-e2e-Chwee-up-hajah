7a11c512fdb2e289352ef689d47720ea660ad11d
unknown <kyung3098@naver.com>
2025-07-08 22:56:59 +0900

    feat: 경매 판매 내역 페이지 (#156)

--- apps/web/app/(main)/auction/my-listings/page.tsx
+++ apps/web/app/(main)/auction/my-listings/page.tsx
@@ -1,12 +1,44 @@
-import { AuctionListings } from '@/widgets/auction-listings/ui/AuctionListings';
+'use client';
 
-import { mockListings } from '@/mock/auction';
+import { useMyListings } from '@/shared/api/client/auction/useMyListings';
+import { mapAuctionItem } from '@/shared/lib/utils/mapAuctionItem';
+import { useAuthStore } from '@/shared/stores/auth';
+import { MyListings } from '@/widgets/my-listings/ui/MyListings';
+import { useState } from 'react';
 
 const Page = () => {
+  const [filter, setFilter] = useState('in-progress');
+  const { userId } = useAuthStore();
+
+  const { data: listings, isLoading, isError } = useMyListings(userId!, filter);
+
+  if (isLoading) {
+    return <div>판매 물품 불러오는 중...</div>;
+  }
+
+  if (isError) {
+    return <div className="my-8 text-red-500">내 판매 물품을 불러오지 못했습니다.</div>;
+  }
+
+  const filteredList = (listings || []).map(mapAuctionItem);
+  console.log(filteredList);
+
   return (
     <main className="text-neutral-70" role="main">
       <h1 className="mb-3 text-base font-semibold">판매중인 물품</h1>
-      <AuctionListings listData={mockListings} />
+      <div className="w-full text-right">
+        <select
+          name="listFilter"
+          id="actionListFilter"
+          className="text-neutral-70 my-2 ml-auto w-1/2 rounded-sm p-1 shadow-md"
+          value={filter}
+          onChange={(e) => setFilter(e.target.value)}
+        >
+          <option value="in-progress">판매중</option>
+          <option value="ended">종료</option>
+        </select>
+      </div>
+      <MyListings listData={filteredList} />
     </main>
   );
 };
