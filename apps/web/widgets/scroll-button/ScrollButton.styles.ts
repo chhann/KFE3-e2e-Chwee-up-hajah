71bdfacbaa1fe514ae688faae37a13b4b6714b10
park-chan-hui <fhfh54321@motionimaging.net>
2025-07-18 21:30:28 +0900

    feat: 최상위 스크롤 버튼 구현

---
+++ apps/web/widgets/scroll-button/ScrollButton.styles.ts
@@ -0,0 +1,5 @@
+export const ScrollButtonStyle = {
+  container: 'z-1 sticky bottom-10 right-0 float-right',
+  button:
+    'rounded-full bg-[var(--button-primary-bg)] p-4 text-[var(--button-primary-text)] hover:bg-[var(--button-primary-bg-hover)]',
+};
