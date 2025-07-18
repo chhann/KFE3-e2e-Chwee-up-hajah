f6fbd704aa15d02af119dd8076e746380f15350b
chhann <wjdcks2252@naver.com>
2025-07-18 23:41:37 +0900

    chore: vercel & google analytics 연동 (#172)

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -7,6 +7,7 @@ import { Providers } from './providers';
 import '@repo/ui/styles';
 import { cookies } from 'next/headers';
 import './globals.css';
+import { Analytics } from '@vercel/analytics/next';
 
 const notoSansKR = Noto_Sans_KR({
   subsets: ['latin'],
@@ -29,6 +30,9 @@ export const metadata: Metadata = {
 import { NotificationPermissionButton } from '@/features/test/NotificationPermissionButton';
 import { PushSubscriptionEffect } from '@/shared/hooks/PushSubscriptionEffect';
 import * as Sentry from '@sentry/nextjs';
+import GoogleAnalytics from '@/shared/hooks/GoogleAnalyticsEffect';
+import Script from 'next/script';
+import { GA_TRACKING_ID } from '@/shared/lib/ga4/gtag';
 
 export default async function RootLayout({ children }: { children: React.ReactNode }) {
   const theme = (await cookies()).get('theme')?.value ?? 'light';
@@ -43,6 +47,22 @@ export default async function RootLayout({ children }: { children: React.ReactNo
           <Providers>
             <PushSubscriptionEffect />
             {children}
+            <Analytics />
+            <GoogleAnalytics />
+            <Script
+              src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+              strategy="afterInteractive"
+            />
+            <Script id="google-analytics" strategy="afterInteractive">
+              {`
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', '${GA_TRACKING_ID}', {
+                  page_path: window.location.pathname,
+                });
+              `}
+            </Script>
           </Providers>
         </Sentry.ErrorBoundary>
       </body>
